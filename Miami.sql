581ea21e72d7573a58e8a1146bcfaeb64a24f17f
Niloofar Soltanmohammadi <niloo.6236@gmail.com>
2026-03-02 11:05:08 +0100

    Add files via upload

---
+++ Miami.sql
@@ -0,0 +1,25 @@
+
+create or replace view retail.table_joined as
+
+SELECT 
+s.date,
+dayname(date) as day_of_week,
+case when weekday(date) in (5,6) then "weekend" else "weekday" end as is_weekend,
+s.shop_id,
+s.shop_name,
+s.customers,
+s.sales_usd,
+s.sales_usd/s.customers as sales_per_customer,
+su.pct_male,
+su.pct_female,
+su.pct_family,
+su.pct_single,
+w.avg_temp_f,
+w.precip_in,
+w.is_rain,
+w.humidity_pct
+FROM retail.sales s
+left join retail.survey su 
+using(date)
+left join retail.weather w
+using(date)
